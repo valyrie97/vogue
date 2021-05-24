2c052b3feeda3cb43643e33e9a1a55fb3924c806
Valerie <marcus@valnet.xyz>
2021-05-23 20:35:48 -0400

    re-instantiate examples/test as system testing

--- examples/test/main.v
+++ examples/test/main.v
@@ -1,5 +1,24 @@
 singleton;
 
-restore {
-	console.log('worked!\n'.repeat(10));
+link counter;
+link window;
+
+async restore {
+	// process.stdout.write(typeof console._link + '\n');
+	console.log('~ Welcome to Vogue ~');
+	// process.stdout.write(JSON.stringify(console, null, 2))
+	counter ??= create('counter', {});
+	for(let i = 0; i < 10; i ++)
+		counter.increment();
+	// const choice = await console.choice('select a thing', ['a', 'b', 'c', 'd']);
+
+	console.log(counter.getCount());
+
+	// window ??= create('SDL.window', {});
+
+	// window.setScene()
+
+	// await counter.render();
+
+	sync();
 }
